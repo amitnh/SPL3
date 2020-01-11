18affb084c1e75509b55f97079f2714ad2962598
amitnh <amitnh@gmail.com>
2020-01-11 16:45:48 +0200

    database and ConnectionsImp updates

--- spl-net/src/main/java/bgu/spl/net/srv/ConnectionsImp.java
+++ spl-net/src/main/java/bgu/spl/net/srv/ConnectionsImp.java
@@ -1,19 +1,33 @@
 package bgu.spl.net.srv;
 
-public class ConnectionsImp implements Connections{
+import java.util.concurrent.ConcurrentHashMap;
 
+public class ConnectionsImp implements Connections{
+    private DataBase dataBase;
+    public ConnectionsImp()
+    {
+        dataBase = dataBase.getInstance();
+
+    }
     @Override
     public boolean send(int connectionId, Object msg) {
+        try{
+            dataBase.getConnectionsHandlers().get(connectionId).send(msg);
+        return true;
+        } catch (Exception ignored){}
         return false;
     }
 
     @Override
     public void send(String channel, Object msg) {
-
+        for(Integer connectionId:dataBase.getTopics().get(channel))
+        {
+            send(connectionId,msg);
+        }
     }
 
     @Override
     public void disconnect(int connectionId) {
-
+        dataBase.getConnectionsHandlers().get(connectionId).close();
     }
 }
--- spl-net/src/main/java/bgu/spl/net/srv/DataBase.java
+++ spl-net/src/main/java/bgu/spl/net/srv/DataBase.java
@@ -6,17 +6,36 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class DataBase {
     private DataBase database = new DataBase();
     private ConcurrentHashMap<String,String> users; // user name , passcode
-    private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> topics;// topic, user names list
+    private ConcurrentHashMap<String,ConcurrentLinkedQueue<Integer>> topics;// topic, user id list
     private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> booksByTopic;// topic, books list
-    private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> clintesBooks;// client, books list
-
-
+    private ConcurrentHashMap<Integer,ConcurrentLinkedQueue<String>> clintesBooks;// user id, books list
+    private ConcurrentHashMap<Integer,NonBlockingConnectionHandler> connectionsHandlers;// ConHandler for each active user (by user's connectionId)
 
     private DataBase(){
         users = new ConcurrentHashMap<>();
         topics = new ConcurrentHashMap<>();
         booksByTopic = new ConcurrentHashMap<>();
         clintesBooks = new ConcurrentHashMap<>();
+        connectionsHandlers= new ConcurrentHashMap<>();
     }
     public DataBase getInstance(){return database;}// singleton
+
+    public ConcurrentHashMap<String, String> getUsers() {
+        return users;
+    }
+
+    public ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> getBooksByTopic() {
+        return booksByTopic;
+    }
+
+    public ConcurrentHashMap<Integer, ConcurrentLinkedQueue<String>> getClintesBooks() {
+        return clintesBooks;
+    }
+
+    public ConcurrentHashMap<Integer, NonBlockingConnectionHandler> getConnectionsHandlers() {
+        return connectionsHandlers;
+    }
+    public ConcurrentHashMap<String,ConcurrentLinkedQueue<Integer>> getTopics() {
+        return topics;
+    }
 }
