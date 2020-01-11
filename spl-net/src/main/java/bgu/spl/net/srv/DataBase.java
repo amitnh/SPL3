5783f13156638fc03bb3a37c534593af0649d96a
amitnh <amitnh@gmail.com>
2020-01-11 16:05:34 +0200

    database

--- spl-net/src/main/java/bgu/spl/net/srv/DataBase.java
+++ spl-net/src/main/java/bgu/spl/net/srv/DataBase.java
@@ -6,11 +6,17 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class DataBase {
     private DataBase database = new DataBase();
     private ConcurrentHashMap<String,String> users; // user name , passcode
-    private ConcurrentHashMap<String,String> topics;// topic, user names list
+    private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> topics;// topic, user names list
+    private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> booksByTopic;// topic, books list
+    private ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> clintesBooks;// client, books list
+
 
 
     private DataBase(){
         users = new ConcurrentHashMap<>();
+        topics = new ConcurrentHashMap<>();
+        booksByTopic = new ConcurrentHashMap<>();
+        clintesBooks = new ConcurrentHashMap<>();
     }
     public DataBase getInstance(){return database;}// singleton
 }
