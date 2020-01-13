609ed3a2c5fb67790553b73b8df55f0edf759fee
amitnh <amitnh@gmail.com>
2020-01-13 19:33:14 +0200

    a liitle bit of DECODE

--- spl-net/src/main/java/bgu/spl/net/api/MessageEncDecImp.java
+++ spl-net/src/main/java/bgu/spl/net/api/MessageEncDecImp.java
@@ -1,17 +1,58 @@
 package bgu.spl.net.api;
 
-import bgu.spl.net.frames.Frame;
+import bgu.spl.net.frames.*;
 
 import java.util.function.Supplier;
 
 public class MessageEncDecImp implements MessageEncoderDecoder, Supplier {
     private byte[] bytes = new byte[1 << 10]; //start with 1k
-
+    private Frame frame;
+    private int state=0,i=0;
     @Override
     public Frame decodeNextByte(byte nextByte) {
+    if (nextByte=='\u0000')
+    {
+        frame.setBody(bytes.toString());
+        bytes= new byte[1 << 10];
+        return frame;
+    }
+    if(i==0)
+    {
+        state=0;
+    }
+    if (state==0)
+    {
+        if (nextByte=='\n')
+        {
+            String command = new String(bytes);
+            switch (command){
+                case "SEND":
+                    frame = new SEND(null,null);
+                    break;
+                case "CONNECT":
+                    frame = new CONNECT(null,null,null);
+                    break;
+                case "SUBSCRIBE":
+                    frame = new SUBSCRIBE(null,null,null);
+                    break;
+                case "UNSUBSCRIBE":
+                    frame = new UNSUBSCRIBE(null,null,null);
+                    break;
+                case "DISCONNECT":
+                    frame = new DISCONNECT(null,null);
+                    break;
+            }
+            state=1;
+        }
+        else
+        {
+            bytes[i] = nextByte;
+        }
+    }
+
 
         return null;
-    } //TODO: copy the encoder decoder from a friend
+    }
 
 //    @Override
 //    public byte[] encode(Frame message) {
